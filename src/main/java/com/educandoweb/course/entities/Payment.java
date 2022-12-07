2aa3be6aa18e0310f59e1edcf314e41870182581
Carlos H <carloshen234@outlook.com>
2022-12-07 19:51:39 -0300

    User insert

--- src/main/java/com/educandoweb/course/entities/Payment.java
+++ src/main/java/com/educandoweb/course/entities/Payment.java
@@ -19,7 +19,6 @@ import jakarta.persistence.Table;
 public class Payment implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
